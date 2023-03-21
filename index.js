3a178f15e13566bc8797c6216de82bd75d68fe6e
arsenhreskiv <harsen17mu@gmail.com>
2023-03-21 08:29:31 -0500

    Made logic for checking available spaces

--- index.js
+++ index.js
@@ -6,33 +6,43 @@
 // let p1q = -1
 // let p2 = 2
 // let p2q = -2
+let board = [
+  [0, 2, 0, 2, 0, 2, 0, 2], // col 0
+  [2, 0, 2, 0, 2, 0, 2, 0], // col 1
+  [0, 2, 0, 2, 0, 2, 0, 2], // col 2
+  [0, 0, 0, 0, 0, 0, 0, 0], // col 3
+  [0, 0, 0, 0, 0, 0, 0, 0], // col 4
+  [1, 0, 1, 0, 1, 0, 1, 0], // col 5
+  [0, 1, 0, 1, 0, 1, 0, 1], // col 6
+  [1, 0, 1, 0, 1, 0, 1, 0], // col 7
+]
 let turn = null
 let rPieces = 12
-let bPeices = 12
+let bPieces = 12
+let selectedPiece = null 
+let player
 
 
+//init()
+// function init() {   
+//     board = [
+//       [0, 2, 0, 2, 0, 2, 0, 2], // col 0
+//       [2, 0, 2, 0, 2, 0, 2, 0], // col 1
+//       [0, 2, 0, 2, 0, 2, 0, 2], // col 2
+//       [0, 0, 0, 0, 0, 0, 0, 0], // col 3
+//       [0, 0, 0, 0, 0, 0, 0, 0], // col 4
+//       [1, 0, 1, 0, 1, 0, 1, 0], // col 5
+//       [0, 1, 0, 1, 0, 1, 0, 1], // col 6
+//       [1, 0, 1, 0, 1, 0, 1, 0], // col 7
+//     ]
+//     //render()
+// }
+ console.log(board[0].length)
 
-init()
+  function canMovePiece(row, col, move) {
 
-function init() {
-   
-    board = [
-      [0, 2, 0, 2, 0, 2, 0, 2], // col 0
-      [2, 0, 2, 0, 2, 0, 2, 0], // col 1
-      [0, 2, 0, 2, 0, 2, 0, 2], // col 2
-      [0, 0, 0, 0, 0, 0, 0, 0], // col 3
-      [0, 0, 0, 0, 0, 0, 0, 0], // col 4
-      [1, 0, 1, 0, 1, 0, 1, 0], // col 5
-      [0, 1, 0, 1, 0, 1, 0, 1], // col 6
-      [1, 0, 1, 0, 1, 0, 1, 0], // col 7
-    ]
-    render()
-}
-
-  function canMovePiece(cor1, cor2, move) {
-
-    if (cor1 >= 0 && cor1 < board.length && cor2 >= 0 && cor2 < board[cor1].length) {
-      if (move === "up-left" && cor1 > 0 && cor2 > 0 && board[cor1-1][cor2-1] === 0) {
+    if (row >= 0 && row < board.length && col >= 0 && col < board[col].length) {
+      if (move === "up-left" && col > 0 && cor2 > 0 && board[cor1-1][cor2-1] === 0) {
         return true;
       } else if (move === "up-right" && cor1 > 0 && cor2 < board[cor1].length-1 && board[cor1-1][cor2+1] === 0) {
         return true;
@@ -41,7 +51,36 @@ function init() {
   }
 }
 
-clickedPieceListener() {
-  if
+function isKing(){
+
 }
 
+function selectedPieces(){
+  if (turn == true) {
+    player = rPieces
+  } else {
+    player = bPieces
+  }
+}
+
+function clickedPieceListener() {
+  if (turn == true) {
+    let i = 0
+    while (i < rPieces.length){
+      rPieces[i].addEventListener('click', selectedPieces)
+    i++
+    }
+  } else {
+    for (let i = 0; i < bPieces; i++){
+      bPieces[i].addEventListener('click', selectedPieces)
+    }
+  }
+}
+
+function availableSpaces() {
+  for(let r = 0; r < board.length; r++ ){
+      for(let c = 0; c < board.length; c++) {
+        
+      }
+  }
+}
